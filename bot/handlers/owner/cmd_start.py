f75f4e69926903590a33c31277c6a6f00e2d717a
Kyubi <cyber.kyubi@gmail.com>
2022-09-21 13:46:54 +0400

    v1.0beta1.2

--- bot/handlers/owner/cmd_start.py
+++ bot/handlers/owner/cmd_start.py
@@ -6,11 +6,16 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from storages.redis.storage import RedisStorage
 from handlers.admin.main_menu.menu import admin__main_menu
 from handlers.user.main_menu.menu import user__main_menu
+from .owner_mode import owner__main_menu
 from handlers.user.get_user_data import get_current_group
 
 
 async def owner__cmd_start(message: Message, state: FSMContext, redis__db_1: RedisStorage):
     owners_data = await redis__db_1.get_data('owners')
+    if not owners_data:
+        await owner__main_menu(message, state)
+        return
+
     user_id = message.from_user.id
     owner = owners_data.get(str(user_id))
     if owner:
