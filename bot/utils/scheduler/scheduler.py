b83887b84b22bc415d7941a9b5334c9f04fd7b7e
Kyubi <cyber.kyubi@gmail.com>
2022-09-26 08:35:55 +0400

    hotfix

--- bot/utils/scheduler/scheduler.py
+++ bot/utils/scheduler/scheduler.py
@@ -10,16 +10,27 @@ class DeleteOldTimetable:
     @staticmethod
     async def delete_old_timetable(redis__db_2: RedisStorage):
         new_timetable = await redis__db_2.get_data('timetable_for_new_week')
+        await redis__db_2.delete_key('timetable_for_new_week')
         if new_timetable:
             await redis__db_2.set_data('timetable', new_timetable)
 
+    # async def start_job(self, redis__db_2):
+    #     self.scheduler.add_job(
+    #         self.delete_old_timetable,
+    #         trigger='cron',
+    #         day_of_week='sun',
+    #         hour=0,
+    #         minute=0,
+    #         timezone='Europe/Samara',
+    #         args=(redis__db_2, )
+    #     )
     async def start_job(self, redis__db_2):
         self.scheduler.add_job(
             self.delete_old_timetable,
             trigger='cron',
-            day_of_week='sun',
-            hour=0,
-            minute=0,
+            day_of_week='mon',
+            hour=8,
+            minute=40,
             timezone='Europe/Samara',
             args=(redis__db_2, )
         )
